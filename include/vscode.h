a933027344b15c324071ff973255ddcd955f073c
jeebuscrossaint <thediamond270@gmail.com>
2026-03-05 17:13:20 -0500

    live vscode reloading

--- include/vscode.h
+++ include/vscode.h
@@ -8,9 +8,6 @@
 #include "tinted_parser.h"
 #include "yaml.h"
 
-// Generate a VSCode theme from a Base16 scheme
-int vscode_generate_theme(const Base16Scheme *scheme, const char *output_path, const FontConfig *font);
-
 // Apply VSCode theme to current VSCode configuration
 int vscode_apply_theme(const Base16Scheme *scheme, const FontConfig *font);
 
